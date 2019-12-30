c903b3f2603411097d3cbc2985127205fd4c38a8
fernandotiger <59361524+fernandotiger@users.noreply.github.com>
2019-12-30 11:14:01 +0000

    Add files via upload

---
+++ src/main/resources/static/js/userDetails.js
@@ -0,0 +1,15 @@
+jQuery(function($) {
+	
+	$.ajax({
+		  url: "https://fiddle.jshell.net/favicon.png",
+		  beforeSend: function( xhr ) {
+		    xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+		  }
+		})
+		  .done(function( data ) {
+		    if ( console && console.log ) {
+		      console.log( "Sample of data:", data.slice( 0, 100 ) );
+		    }
+		  });
+	
+});
